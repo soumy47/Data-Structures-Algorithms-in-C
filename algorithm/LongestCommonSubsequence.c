18ad67e41e9e50d2862f120b0a562f0a2aced3f1
sans2001 <87486013+sans2001@users.noreply.github.com>
2021-10-02 12:49:28 +0530

    Update LongestCommonSubsequence.c

--- algorithm/LongestCommonSubsequence.c
+++ algorithm/LongestCommonSubsequence.c
@@ -2,55 +2,31 @@
 #include<string.h>
  
 int i,j,m,n,c[20][20];
-char x[20],y[20],b[20][20];
+char x[20],y[20];
  
-int print(int i,int j)
+int lcs()
 {
-                if(i==0 || j==0)
-                                return 0;
-                
-                if(b[i][j]=='c')
-                {
-                                print(i-1,j-1);
-                                printf("%c",x[i-1]);
-                }
-                else if(b[i][j]=='u')
-                                print(i-1,j);
+        m=strlen(x);
+        n=strlen(y);
+        for(i=0;i<=m;i++)
+                        c[i][0]=0;
+        for(i=0;i<=n;i++)
+                        c[0][i]=0;
+                                        
+        for(i=1;i<=m;i++)
+            for(j=1;j<=n;j++)
+            {
+                if(x[i-1]==y[j-1])
+                	c[i][j]=c[i-1][j-1]+1;
+                    
+                else if(c[i-1][j]>=c[i][j-1])
+                	c[i][j]=c[i-1][j];
+                    
                 else
-                                print(i,j-1);
-}
- 
-void lcs()
-{
-                m=strlen(x);
-                n=strlen(y);
-                for(i=0;i<=m;i++)
-                                c[i][0]=0;
-                for(i=0;i<=n;i++)
-                                c[0][i]=0;
-                
-                ///c denotes cross
-				//u denotes upward direction
-				//l denotes downward directions                                
-                for(i=1;i<=m;i++)
-                    for(j=1;j<=n;j++)
-                    {
-                        if(x[i-1]==y[j-1])
-                        {
-                            c[i][j]=c[i-1][j-1]+1;
-                            b[i][j]='c';
-                        }
-                        else if(c[i-1][j]>=c[i][j-1])
-                        {
-                            c[i][j]=c[i-1][j];
-                            b[i][j]='u';
-                        }
-                        else
-                        {
-                            c[i][j]=c[i][j-1];
-                            b[i][j]='l';
-                        }
-                    }
+                	c[i][j]=c[i][j-1];
+                   
+            }
+        return c[m][n];
               
 }
 int main()
@@ -61,8 +37,7 @@ int main()
                 scanf("%s",y);
                 m=strlen(x);
                 n=strlen(y);
-                printf("\nThe Longest Common Subsequence is ");
-                lcs();
-              	print(m,n);
+                printf("\nThe Longest Common Subsequence is: %d ",lcs());
+              
 return 0;
 }
